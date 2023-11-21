ba76d52b73510498e2ecae440b250072db0f1229
PavlinaRacakova <pavlina.kupkova@email.cz>
2023-11-21 19:35:37 +0700

    Messages.class added

---
+++ src/main/java/cz/czechitas/java2webapps/ukol6/Messages.java
@@ -0,0 +1,16 @@
+package cz.czechitas.java2webapps.ukol6;
+
+public class Messages {
+
+    public static String getInformationNotProvidedMessage() {
+        return "n/a";
+    }
+
+    public static String getNewBusinessCardTitle() {
+        return "Save new business card";
+    }
+
+    public static String getEditBusinessCardTitle() {
+        return "Edit existing business card";
+    }
+}
--- src/main/java/cz/czechitas/java2webapps/ukol6/controller/BusinessCardController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/BusinessCardController.java
@@ -1,5 +1,6 @@
 package cz.czechitas.java2webapps.ukol6.controller;
 
+import cz.czechitas.java2webapps.ukol6.Messages;
 import cz.czechitas.java2webapps.ukol6.entity.BusinessCard;
 import cz.czechitas.java2webapps.ukol6.repository.BusinessCardRepository;
 import jakarta.validation.Valid;
@@ -17,10 +18,6 @@ public class BusinessCardController {
 
     private final BusinessCardRepository repository;
 
-    private final String informationNotProvidedMessage = "n/a";
-    private final String newBusinessCardTitle = "Save new business card";
-    private final String editBusinessCardTitle = "Edit existing business card";
-
     public BusinessCardController(BusinessCardRepository repository) {
         this.repository = repository;
     }
@@ -34,7 +31,7 @@ public class BusinessCardController {
     public ModelAndView list() {
         ModelAndView result = new ModelAndView("list");
         result.addObject("businessCardsList", repository.findAll());
-        result.addObject("informationNotProvided", informationNotProvidedMessage);
+        result.addObject("informationNotProvided", Messages.getInformationNotProvidedMessage());
         return result;
     }
 
@@ -43,7 +40,7 @@ public class BusinessCardController {
         ModelAndView result = new ModelAndView("detail");
         result.addObject("businessCard", repository.findById(id).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
-        result.addObject("informationNotProvided", informationNotProvidedMessage);
+        result.addObject("informationNotProvided", Messages.getInformationNotProvidedMessage());
         return result;
     }
 
@@ -51,14 +48,14 @@ public class BusinessCardController {
     public ModelAndView showNewBusinessCardForm() {
         ModelAndView result = new ModelAndView("businessCardForm");
         result.addObject("businessCard", new BusinessCard());
-        result.addObject("title", newBusinessCardTitle);
+        result.addObject("title", Messages.getNewBusinessCardTitle());
         return result;
     }
 
     @PostMapping("/new")
     public Object addNewBusinessCard(@Valid @ModelAttribute BusinessCard businessCard, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return new ModelAndView("businessCardForm").addObject("title", newBusinessCardTitle);
+            return new ModelAndView("businessCardForm").addObject("title", Messages.getNewBusinessCardTitle());
         }
         repository.save(businessCard);
         return "redirect:/";
@@ -75,14 +72,14 @@ public class BusinessCardController {
         ModelAndView result = new ModelAndView("businessCardForm");
         result.addObject("businessCard", repository.findById(id).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
-        result.addObject("title", editBusinessCardTitle);
+        result.addObject("title", Messages.getEditBusinessCardTitle());
         return result;
     }
 
     @PostMapping("/edit/{id}")
     public Object editExistingBusinessCard(@Valid @ModelAttribute BusinessCard businessCard, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return new ModelAndView("businessCardForm").addObject("title", editBusinessCardTitle);
+            return new ModelAndView("businessCardForm").addObject("title", Messages.getEditBusinessCardTitle());
         }
         repository.save(businessCard);
         return "redirect:/detail/{id}";
