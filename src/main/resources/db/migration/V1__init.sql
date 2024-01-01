0718871bc64d475221945381e451f29b0c788c6d
PavlinaRacakova <pavlina.kupkova@email.cz>
2024-01-01 17:19:52 +0100

    database script error fixed

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,18 +1,18 @@
-CREATE TABLE vizitka
+CREATE TABLE BUSINESS_CARD
 (
     id         IDENTITY PRIMARY KEY,
-    cele_jmeno VARCHAR(100) NOT NULL,
-    firma      VARCHAR(100) NOT NULL,
-    ulice      VARCHAR(100) NOT NULL,
-    obec       VARCHAR(100) NOT NULL,
-    psc        CHAR(5)      NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    company      VARCHAR(100) NOT NULL,
+    street      VARCHAR(100) NOT NULL,
+    town       VARCHAR(100) NOT NULL,
+    zip_code        CHAR(5)      NOT NULL,
     email      VARCHAR(100),
-    telefon    VARCHAR(20),
+    phone_number    VARCHAR(20),
     web        VARCHAR(100)
 );
 
 
-INSERT INTO vizitka (cele_jmeno, firma, ulice, obec, psc, email, telefon, web)
+INSERT INTO BUSINESS_CARD (name, company, street, town, zip_code, email, phone_number, web)
 VALUES ('Dita (Přikrylová) Formánková', 'Czechitas z. s.', 'Václavské náměstí 837/11', 'Praha 1', '11000', 'dita@czechitas.cs', '+420 800123456',
         'www.czechitas.cz'),
        ('Barbora Bühnová', 'Czechitas z. s.', 'Václavské náměstí 837/11', 'Praha 1', '11000', NULL, '+420 800123456', 'www.czechitas.cz'),
